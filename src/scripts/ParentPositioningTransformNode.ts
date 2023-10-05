30d42b20f16481e47c93af4163a2fdb5dca428d5
Hyunjoong Kim <eclvisltion@protonmail.com>
2023-10-05 16:25:18 +0900

    방금 올린 것 중 누락된거 마저 올림

--- src/scripts/ParentPositioningTransformNode.ts
+++ src/scripts/ParentPositioningTransformNode.ts
@@ -1,6 +1,6 @@
 import { Scene, TransformNode } from "@babylonjs/core";
 
-export class ParentPositioningTransformNode extends TransformNode {
+export class TransformNode_pos extends TransformNode {
     constructor (name: string, scene: Scene, parent: TransformNode) {
         super(name, scene);
         super.setParent(parent);
